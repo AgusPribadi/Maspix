39068ba395f4a5661cf9b072a0f4f3dd9693d763
Agus Pribadi <aguspribadi368@gmail.com>
2023-09-09 17:32:31 +0700

    mengupdate fitur gallery dengan LightGallery, lalu bagi 3 section portfolio, dan perbaiki footer

--- aplikasi_maspix/models.py
+++ aplikasi_maspix/models.py
@@ -1,11 +1,16 @@
-# aplikasi_maspix/models.py
-
 from django.db import models
 
 class Portfolio(models.Model):
+    CATEGORY_CHOICES = (
+        ('Website', 'Website'),
+        ('Desain', 'Desain'),
+        ('Machine Learning', 'Machine Learning'),
+    )
+
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='portfolio_images/')
     description = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, null=True, blank=True)
 
     def __str__(self):
         return self.title
